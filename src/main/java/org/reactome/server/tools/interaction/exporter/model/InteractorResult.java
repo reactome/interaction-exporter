b4db0713c07aca305aed7da2f5b7010a6c8b569d
Pascual Lorente <pasculorente@gmail.com>
2018-04-10 12:42:44 +0100

    get interactor data using one cypher query

---
+++ src/main/java/org/reactome/server/tools/interaction/exporter/model/InteractorResult.java
@@ -0,0 +1,89 @@
+package org.reactome.server.tools.interaction.exporter.model;
+
+import org.reactome.server.tools.interaction.exporter.psi.SimpleCrossReference;
+import org.reactome.server.tools.interaction.exporter.psi.SimpleFeature;
+import psidev.psi.mi.tab.model.Alias;
+import psidev.psi.mi.tab.model.AliasImpl;
+import psidev.psi.mi.tab.model.CrossReference;
+import psidev.psi.mi.tab.model.Feature;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class InteractorResult {
+
+	private String schemaClass;
+	private String referenceType;
+	private List<CrossReference> identifiers = Collections.emptyList();
+	private List<Alias> aliases = Collections.emptyList();
+	private List<CrossReference> species = Collections.emptyList();
+	private List<Feature> features = Collections.emptyList();
+	private List<CrossReference> crossReferences = Collections.emptyList();
+
+	public void setSchemaClass(String schemaClass) {
+		this.schemaClass = schemaClass;
+	}
+
+	public void setReferenceType(String referenceType) {
+		this.referenceType = referenceType;
+	}
+
+	public void setIdentifiers(List<SimpleCrossReference> identifiers) {
+		this.identifiers = new LinkedList<>(identifiers);
+	}
+
+	public void setAliases(List<AliasImpl> aliases) {
+		this.aliases = new LinkedList<>(aliases);
+	}
+
+	public void setSpecies(List<SimpleCrossReference> species) {
+		this.species = new LinkedList<>(species);
+	}
+
+	public void setFeatures(List<SimpleFeature> features) {
+		this.features = new LinkedList<>(features);
+	}
+
+	public void setCrossReferences(List<SimpleCrossReference> crossReferences) {
+		this.crossReferences = new LinkedList<>(crossReferences);
+	}
+
+	public String getSchemaClass() {
+		return schemaClass;
+	}
+
+	public List<CrossReference> getIdentifiers() {
+		return identifiers;
+	}
+
+	public List<Alias> getAliases() {
+		return aliases;
+	}
+
+	public List<CrossReference> getSpecies() {
+		return species;
+	}
+
+	public List<Feature> getFeatures() {
+		return features;
+	}
+
+	public List<CrossReference> getCrossReferences() {
+		return crossReferences;
+	}
+
+	public String getReferenceType() {
+		return referenceType;
+	}
+
+	@Override
+	public String toString() {
+		return schemaClass + "(" + referenceType + ")"
+				+ "{identifiers: " + identifiers.toString()
+				+ ", aliases: " + aliases.toString()
+				+ ", species: " + species.toString()
+				+ ", features: " + features.toString()
+				+ ", crossReferences: " + crossReferences.toString();
+	}
+}
---
+++ src/main/java/org/reactome/server/tools/interaction/exporter/psi/GraphHelper.java
@@ -0,0 +1,108 @@
+package org.reactome.server.tools.interaction.exporter.psi;
+
+import org.apache.commons.collections.map.LRUMap;
+import org.reactome.server.graph.exception.CustomQueryException;
+import org.reactome.server.graph.service.AdvancedDatabaseObjectService;
+import org.reactome.server.graph.utils.ReactomeGraphCore;
+import org.reactome.server.tools.interaction.exporter.model.ContextResult;
+import org.reactome.server.tools.interaction.exporter.model.InteractorResult;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class GraphHelper {
+
+	private static final AdvancedDatabaseObjectService SERVICE = ReactomeGraphCore.getService(AdvancedDatabaseObjectService.class);
+	private static final String INTERACTOR_QUERY = "" +
+			"MATCH (e:DatabaseObject{stId:{stId}}) " +
+			"OPTIONAL MATCH (e)-[:species]->(species) " +
+			"WITH e, collect(species) AS species " +
+			"OPTIONAL MATCH (e)-[:referenceEntity]->(re)-[:crossReference]->(recr) " +
+			"WITH e, re, species, collect(recr) AS cr " +
+			"OPTIONAL MATCH (e)-[:crossReference]->(ecr) " +
+			"WITH e, re, species, cr + collect(ecr) AS cr " +
+			"OPTIONAL MATCH (e)-[:compartment]->(comp) " +
+			"WITH e, re, species, cr, collect(comp) AS comp " +
+			"OPTIONAL MATCH (e)-[:hasModifiedResidue]-(mr)-[:psiMod]->(psi) " +
+			"RETURN e.schemaClass AS schemaClass, e.referenceType AS referenceType," +
+			"" + // Identifiers: (reactome:stId, re(db,id), cr(db,id), re.otherIdentifier, re.otherIdentifiers)
+			"    [{database:\"reactome\", identifier:e.stId}] " +
+			"    + CASE WHEN cr IS NULL" +
+			"    THEN []" +
+			"    ELSE [ref IN cr | {database:ref.databaseName, identifier:ref.identifier}] END " +
+			"    + CASE WHEN re IS NULL" +
+			"    THEN [] " +
+			"    ELSE [{database:re.databaseName, identifier:re.identifier}]" +
+			"        + CASE WHEN re.otherIdentifier IS NULL " +
+			"        THEN [] " +
+			"        ELSE [id IN re.otherIdentifier WHERE id STARTS WITH \"ENSG\" | {database:\"ENSEMBL\", identifier:id}]  " +
+			"            + [id IN re.otherIdentifier WHERE id STARTS WITH \"EntrezGene:\" | {database:\"entrezgene/locuslink\", identifier:split(id, \":\")[1]}]" +
+			"        END" +
+			"    END AS identifiers, " +
+			"" + // Alias (entity.name)
+			"    [name IN e.name WHERE NOT name CONTAINS \"\\n\" AND NOT name CONTAINS \":\" | {dbSource:\"reactome\", name:name, aliasType:\"name\"}] AS aliases, " +
+			"" +   // features (psi)
+			"    CASE WHEN psi IS NULL" +
+			"    THEN []" +
+			"    ELSE collect({featureType:psi.name[0], text:\"MOD:\" + psi.identifier,range:CASE WHEN mr.coordinate IS NULL THEN [] ELSE [mr.coordinate + \"-\" + mr.coordinate] END}) " +
+			"    END AS features, " +
+			"" +  // species
+			"    [sp IN species | {database:\"taxid\", identifier:sp.taxId, text:sp.name[0]}] AS species, " +
+			"" + // crossReferences
+			"    [c IN comp | {database:c.databaseName, identifier:c.accession, text:c.name}] AS crossReferences";
+	private static final String CONTEXT_QUERY = "" +
+			"MATCH (context:DatabaseObject{stId:{stId}}) " +
+			" OPTIONAL MATCH (context)-[:species]->(species) " +
+			" WITH context, collect(species) AS species " +
+			" OPTIONAL MATCH (context)-[:literatureReference]-(publication:LiteratureReference) " +
+			" WITH context, species, collect(publication) AS publications " +
+			" OPTIONAL MATCH (context)-[:compartment]->(compartment) " +
+			" WITH context, species, publications, collect(compartment) AS compartments " +
+			" OPTIONAL MATCH (context)-[:catalystActivity]->(ca)-[:activity]-(activity)" +
+			" OPTIONAL MATCH (context)<-[:inferredTo]-(inferred) " +
+			" OPTIONAL MATCH (context)<-[:created]-(created:InstanceEdit) " +
+			" OPTIONAL MATCH (context)<-[:modified]-(modified:InstanceEdit) " +
+			" RETURN created.dateTime AS created, modified.dateTime AS modified, " +
+			"    CASE WHEN species IS NULL" +
+			"        THEN []" +
+			"        ELSE [sp IN species | {database:\"taxid\", identifier:sp.taxId, text:sp.name[0]}]" +
+			"    END AS species," +
+			"    publications, context.schemaClass as schemaClass," +
+			"    inferred IS NOT NULL AS inferred," +
+			"    CASE WHEN ca IS NULL" +
+			"        THEN []" +
+			"        ELSE [{database:activity.databaseName, identifier:activity.accession, text:activity.name}]" +
+			"    END" +
+			"    + [c IN compartments | {database:c.databaseName, identifier:c.accession, text:c.name}] AS crossReferences";
+	private static Map INTERACTOR_CACHE = new LRUMap(50);
+	private static Map CONTEXT_CACHE = new LRUMap(10);
+
+
+	public static InteractorResult interactor(String stId) {
+		return (InteractorResult) INTERACTOR_CACHE.computeIfAbsent(stId, GraphHelper::queryInteractor);
+	}
+
+	private static InteractorResult queryInteractor(Object stId) {
+		try {
+			return SERVICE.singletonCustomQuery(InteractorResult.class, INTERACTOR_QUERY, Collections.singletonMap("stId", stId));
+		} catch (CustomQueryException e) {
+
+			// TODO: 10/04/18 create exception Database
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static ContextResult context(String stId) {
+		return (ContextResult) CONTEXT_CACHE.computeIfAbsent(stId, GraphHelper::queryContext);
+	}
+
+	private static ContextResult queryContext(Object stableIdentifier) {
+		try {
+			return SERVICE.singletonCustomQuery(ContextResult.class, CONTEXT_QUERY, Collections.singletonMap("stId", stableIdentifier));
+		} catch (CustomQueryException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
